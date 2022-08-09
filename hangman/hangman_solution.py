ce175abd9c9288749a30b4b3e8b999d4d7c58faa
Ahmad <110533174+alalti@users.noreply.github.com>
2022-08-09 22:50:01 +0100

    edits some code for final debugging

--- hangman/hangman_solution.py
+++ hangman/hangman_solution.py
@@ -1,11 +1,12 @@
+from os import set_blocking
 import random
 
 class Hangman:
 
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
-        self.word_guessed = [len(list(self.word)) * ["_"]]
-        self.num_letters = len(list(self.word))
+        self.word_guessed = len(self.word) * ["_"]
+        self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.list_letters = []
 
@@ -13,13 +14,17 @@ class Hangman:
         print(self.word_guessed)
 
     def check_letter(self, letter):
+        letter = letter.lower()
 
         if letter in list(self.word):
-            self.word_guessed[list(self.word).index(letter)] = letter
-            self.num_letters -= 1
+            self.list_letters.append(letter)
+            for i in range(0, len(self.word)):
+                if self.word[i] == letter:
+                    self.word_guessed[i] = letter
+                    self.num_letters -= 1
             print(f"'Nice! {letter} is in the word!")
             print(self.word_guessed)
-
+            
         elif letter not in list(self.word):
             self.list_letters.append(letter)
             self.num_lives -= 1
@@ -28,35 +33,30 @@ class Hangman:
 
     def ask_letter(self):
 
-        print("Let's play! Please enter your letter")
-
-        letter = input()
-
         while True:
-            if len(letter) > 1:
+            letter = input("Please enter your letter: ")
+            if len(letter) != 1:
                 print("Please, enter just one character")
-                letter = input()
 
-            elif letter in self.list_letter:
+            elif letter in self.list_letters:
                 print(f"{letter} was already tried")
                     
             else:
-                self.list_letter.append(str.lower(letter))
+                self.check_letter(letter)
                 break
-
-        letter.checkletter(letter)
-
+        
 def play_game(word_list):
     game = Hangman(word_list, num_lives=5)
-    game.ask_letter()
-
-    while "_" in game.word_guessed:
+    while True:
         game.ask_letter()
-
-    if game.num_letters == 0:
-        print("Congratulations, you won!")
-    elif game.num_lives == 0:
-        print(f"You ran out of lives. The word was {game.word}")
+        
+        if '_' not in game.word_guessed:
+            print("Congratulations, you won!")
+            break
+        
+        elif game.num_lives == 0:
+            print(f"You ran out of lives. The word was {game.word}")
+            break
 
 if __name__ == '__main__':
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
